5ea828ef0885a323bec381a5004e64b2fe6fc7c1
Matteo Villosio <github.v5331@passmail.net>
2023-12-15 23:16:43 +0100

    some old code, tomorrow I upload the real one

--- service/haystack_documentation_pipeline.py
+++ service/haystack_documentation_pipeline.py
@@ -3,27 +3,20 @@ import os
 from haystack import Pipeline
 from haystack.agents.base import ToolsManager
 from haystack.agents.memory import ConversationSummaryMemory
-from haystack.nodes import PromptNode, WebRetriever, PreProcessor, TopPSampler, Shaper, PromptTemplate
+from haystack.nodes import PromptNode, WebRetriever, PreProcessor, Docs2Answers, DiversityRanker
 from haystack.agents import Agent, Tool
-from haystack.pipelines import WebQAPipeline
-from haystack_memory.memory import MemoryRecallNode
-from haystack_memory.utils import MemoryUtils
 
 
+def resolver_function(query, agent, agent_step):
+    return {
+        "query": query,
+        "tool_names_with_descriptions": agent.tm.get_tool_names_with_descriptions(),
+        "transcript": agent_step.transcript,
+        "memory": agent.memory.load(),
+    }
 
 
-def return_haystack_documentation_agent():
-    openai_key = os.environ['OPENAI_KEY']
-    serperdev_api_key = os.environ['SERPERDEV_API_KEY']
-
-    agent_prompt_node = PromptNode(
-        "gpt-3.5-turbo",
-        api_key=openai_key,
-        stop_words=["Observation:"],
-        model_kwargs={"temperature": 0.05},
-        max_length=8095,
-    )
-
+def return_retriever():
     preprocessor = PreProcessor(
         split_by="word",
         split_length=512,
@@ -31,33 +24,92 @@ def return_haystack_documentation_agent():
         split_overlap=20,
     )
 
-    haystack_documentation_search = WebRetriever(
-        api_key=serperdev_api_key,
+    return WebRetriever(
+        api_key=os.environ['SERPERDEV_API_KEY'],
         allowed_domains=["docs.haystack.deepset.ai"],
         mode="preprocessed_documents",
         preprocessor=preprocessor,
-        top_search_results=10,
+        top_search_results=20,
+        top_k=10,
+    )
+
+
+def generate_pipeline():
+    docs2answers = Docs2Answers()
+
+    ranker = DiversityRanker(
+        model_name_or_path="all-MiniLM-L6-v2",
         top_k=5,
+        similarity="dot_product",
     )
 
-    return Agent(
-        prompt_node=agent_prompt_node,
-        prompt_template='',
-        prompt_parameters_resolver='resolver_function',
-        tools_manager=ToolsManager(['haystack_documentation_search_tool']),
+    haystack_docs = Pipeline()
+    haystack_docs.add_node(component=return_retriever(), name="retriever", inputs=["Query"])
+    haystack_docs.add_node(component=ranker, name="ranker", inputs=["retriever"])
+    return haystack_docs
+
+
+def return_haystack_documentation_agent():
+    openai_key = os.environ['OPENAI_KEY']
+
+    haystack_search_tool = Tool(
+        name="haystack_documentation_search_tool",
+        pipeline_or_node=generate_pipeline(),
+        description="Searches the Haystack documentation for answers to your questions.",
+        output_variable="documents",
     )
 
-    # https://haystack.deepset.ai/tutorials/25_customizing_agent
+    agent_prompt = """
+    In the following conversation, a human user interacts with an AI Agent. This agent is specialises in answering questions about the Haystack documentation.
+    The human user poses questions, and the AI Agent goes through several steps to provide well-informed answers. The good outcome of this conversation is of paramount importance for the wellbeing of the human.
+    The AI Agent must use the available tools to find the up-to-date information. The final answer to the question should be truthfully based solely on the output of the tools.
+    The AI Agent should always be focused on answering the human user's question avoiding any irrelevant information. The agent should provide as many examples as possible, especially code examples.
+    The AI Agent should ignore its knowledge when answering the questions.
+    The AI Agent has access to these tools:
+    {tool_names_with_descriptions}
+
+    The following is the previous conversation between a human and The AI Agent:
+    {memory}
+
+    AI Agent responses must start with one of the following:
+
+    Thought: [the AI Agent's reasoning process]
+    Tool: [tool names] (on a new line) Tool Input: [input as a question for the selected tool WITHOUT quotation marks and on a new line] (These must always be provided together and on separate lines.)
+    Observation: [tool's result]
+    Final Answer: [final answer to the human user's question] (on a new line) ++##++##
+    
+    When selecting a tool, the AI Agent must provide both the "Tool:" and "Tool Input:" pair in the same response, but on separate lines.
+
+    The AI Agent should not ask the human user for additional information, clarification, or context.
+    If the AI Agent cannot find a specific answer after exhausting available tools and approaches, it answers with Final Answer: inconclusive
+
+    Question: {query}
+    Thought:
+    {transcript}
+    """
+
+    agent_prompt_node = PromptNode(
+        "gpt-4",
+        api_key=openai_key,
+        stop_words=["Observation:"],
+        model_kwargs={"temperature": 0.05},
+        max_length=4096,
+    )
+
+    memory_node = return_memory_node(openai_key)
+
+    agent = Agent(
+        agent_prompt_node,
+        prompt_template=agent_prompt,
+        prompt_parameters_resolver=resolver_function,
+        memory=memory_node,
+        tools_manager=ToolsManager([haystack_search_tool]),
+        final_answer_pattern=r"Final Answer\s*:\s*(.*)",
+    )
+
+    return agent
 
 
-class CustomAgent:
-    def __init__(self):
-        self.agent, self.working_memory, self.sensory_memory = return_haystack_documentation_agent()
-        self.memory_utils = MemoryUtils(
-            working_memory=self.working_memory,
-            sensory_memory=self.sensory_memory,
-            agent=self.agent,
-        )
-
-    def run(self, query):
-        return self.memory_utils.chat(query)
+def return_memory_node(openai_key):
+    memory_prompt_node = PromptNode('gpt-3.5-turbo', api_key=openai_key, max_length=2048)
+    return ConversationSummaryMemory(memory_prompt_node)
